c7ae0859b2b63c05794b311afc5ac6aae8630f6c
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2025-01-02 22:41:28 +0530

    Add files via upload

--- K_KUMAR(DSA)2025/BABY STEP/c1.cpp
+++ K_KUMAR(DSA)2025/BABY STEP/c1.cpp
@@ -2,7 +2,33 @@
 using namespace std;
 int main(){
 
-    int x,z,y;
-    cin>>x>>z>>y;
-    cout<<x<<" "<<z<<" "<<y<<endl;
+    // int x,z,y;
+    // cin>>x>>z>>y;
+    // cout<<x<<" "<<z<<" "<<y<<endl;
+
+    // string s = " my name is sagar Manna";
+    // cout<<s<<endl;
+
+    // string m ;
+    //  getline(cin,m);
+    //  cout<<m<<endl;
+    //  return 0;
+
+    int a;
+    cout<<" Enter the Number: ";
+    cin>>a;
+    // if(a==2){
+    //     cout<<"Hello today's  Date: 2 JANUARY";
+    // } else{
+    //     cout<<"Hello Today's Date : 1 JANUARY";
+    // }
+
+    // ternary Operator 
+    // syntax : (condition)? True:False;
+
+    (a==2)?cout<<"Hello today's  Date: 2 JANUARY": cout<<"Hello Today's Date Invalid";
+
+    
+
+
 }
