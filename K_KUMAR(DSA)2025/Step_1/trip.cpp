c70c10b6e0c9d8a2e068028dfc9be5dfdd560de3
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2025-01-17 00:08:37 +0530

    Add files via upload

---
+++ K_KUMAR(DSA)2025/Step_1/trip.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int arr[]={1,2,3,4,5,6};
+
+int n =sizeof(arr)/sizeof(arr[0]);
+for(int i =0; i<n-2;i++){
+    for(int j =i+1;j<n;j++){
+        for(int k=j+1;k<n;k++){
+            int sum=arr[i]+arr[j]+arr[k];
+            cout<<sum<<" ";
+        }
+    }
+}
+
+}
