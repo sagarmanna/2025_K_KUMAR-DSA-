46b52fb8a072f21bca04ee8a3af3123d4c5914cb
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2025-01-16 22:40:15 +0530

    Add files via upload

--- K_KUMAR(DSA)2025/Step_1/pair.cpp
+++ K_KUMAR(DSA)2025/Step_1/pair.cpp
@@ -8,11 +8,10 @@ int n=sizeof(arr)/sizeof(arr[0]);
 for(int i =0;i <n;i++){
     for(int j =i+1;j <n;j++){
         int sum_pair=arr[i]+arr[j];
-        cout<<sum_pair<<" ";
+        cout<<sum_pair<<"  ";
     }
 }
 
 
+
 }
-
-
